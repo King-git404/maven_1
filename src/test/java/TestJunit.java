3af210eed99c33a4129dc24db43ffcfbde5a00c5
12613 <1261337209@qq.com>
2021-03-24 21:18:23 +0800

    maven第一次项目

---
+++ src/test/java/TestJunit.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+/**
+ * @program: maven_1
+ * @description:
+ * @author: King
+ * @create: 2021-03-24 21:04
+ */
+public class TestJunit {
+
+    @Test
+    public void plusTest() {
+        System.out.println("Hello Test ");
+    }
+}
